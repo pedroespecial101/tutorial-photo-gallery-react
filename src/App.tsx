1555d3cb35ece7c913f8af0144d872069b96807d
Peter Treadaway <128326013+pedroespecial101@users.noreply.github.com>
2025-06-18 19:08:26 +0100

    feat: update tab icons and labels for barcode scanner and SKU upload

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { images, square, triangle } from 'ionicons/icons';
+import { images, barcode, cloudUpload } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -56,7 +56,7 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
+            <IonIcon aria-hidden="true" icon={barcode} />
             <IonLabel>Barcode Scanner</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
@@ -64,8 +64,8 @@ const App: React.FC = () => (
             <IonLabel>Photos</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Pete's Tab</IonLabel>
+            <IonIcon aria-hidden="true" icon={cloudUpload} />
+            <IonLabel>Upload SKU</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
